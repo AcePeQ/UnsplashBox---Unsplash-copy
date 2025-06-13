48a0d6d3c73871b66f5d919ac7a68c4982ee92dc
AcePeQ <maciej.nojszewski22@gmail.com>
2025-06-13 17:31:04 +0200

    feat: add toast container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { lazy } from "react";
+import { ToastContainer, Zoom } from "react-toastify";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Layout from "./components/Layouts/Layout";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const SearchPage = lazy(() => import("./pages/SearchPage/SearchPage"));
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 0 } },
+});
 
 function App() {
   return (
@@ -21,6 +24,13 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        theme="light"
+        pauseOnHover
+        transition={Zoom}
+      />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
--- frontend/src/features/imageSearch/useDownloadImage.ts
+++ frontend/src/features/imageSearch/useDownloadImage.ts
@@ -1,11 +1,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { downloadImageApi } from "../../api/imageApi";
+import { toast } from "react-toastify";
 
 export function useDownloadImage() {
   const { mutate: downloadImage } = useMutation({
     mutationFn: ({ download_location }: { download_location: string }) =>
       downloadImageApi(download_location),
-    onError: () => {},
+    onSuccess: () => toast.success("Download successfully"),
+    onError: (error) => toast.error(error.message),
   });
 
   return { downloadImage };
