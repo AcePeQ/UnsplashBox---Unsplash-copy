0e0e8e63606f0205c0695e5ba4cc3c495b5805da
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-03 20:32:22 +0200

    feat: add header media queries
    TODO:
    Create new component for header mobile menu for navigation

--- frontend/src/components/MobileHeader/MobileHeader.tsx
+++ frontend/src/components/MobileHeader/MobileHeader.tsx
@@ -3,29 +3,20 @@ import { useUserStore } from "../../stores/useUserStore";
 import Logo from "../Logo/Logo";
 import NavigationMain from "../NavigationMain/NavigationMain";
 import UserMenu from "../UserMenu/UserMenu";
-import { useMediaQuery } from "react-responsive";
+import styles from "./MobileHeader.module.css";
 
-import styles from "./Header.module.css";
-import MobileHeader from "../MobileHeader/MobileHeader";
-
-function Header() {
+function MobileHeader() {
   const isUser = useUserStore((state) => state.user);
-
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 750px)" });
-
-  if (isTabletOrMobile) {
-    return <MobileHeader />;
-  }
 
   return (
     <header className={styles.header}>
       <Logo />
 
-      <NavigationMain />
-
       {isUser ? <UserMenu /> : <AuthButtons />}
+
+      {/* <NavigationMain /> */}
     </header>
   );
 }
 
-export default Header;
+export default MobileHeader;
